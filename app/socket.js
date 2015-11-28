0c5541cf9a743de76ceb41377cd909e307d43434
Adam Terlson <adam.terlson@gmail.com>
2015-11-28 01:13:52 +0100

    change to require

--- app/socket.js
+++ app/socket.js
@@ -2,7 +2,7 @@ import react from 'react-native';
 import store from './store';
 
 window.navigator.userAgent = 'react-native';
-import io from 'socket.io-client/socket.io';
+let io = require('socket.io-client/socket.io');
 
 const socket = io('192.168.2.35:3000', { jsonp: false });
 
